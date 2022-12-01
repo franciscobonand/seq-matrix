185fbe380b5e73d3f54ccc99caedfdb1aeddb0a6
Francisco Andrade <francisco@inspr.com>
2022-12-01 00:38:32 -0300

    WIP: setup all possible sets of sequences

--- server/entity/sequence.go
+++ server/entity/sequence.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 type Sequences struct {
@@ -10,80 +11,99 @@ type Sequences struct {
 }
 
 func (s Sequences) Validate() (bool, error) {
-	graph := make([][]string, len(s.Letters))
 	rg, _ := regexp.Compile(`^[BUDH]+$`)
 
-	for i, val := range s.Letters {
+	for _, val := range s.Letters {
 		// checks if sequences contain only valid letters and length
 		if match := rg.MatchString(val); !match || len(val) != len(s.Letters) {
 			return false, fmt.Errorf("invalid input: '%s'", val)
 		}
-		graph[i] = []string{val}
 	}
 
-	if countValidSequences(graph) >= 2 {
-		return true, nil
-	}
-
-	return false, nil
+	return countValidSequences(s.Letters) >= 2, nil
 }
 
-func countValidSequences(graph [][]string) int {
+func countValidSequences(seqs []string) int {
 	validSeqs := 0
 
-	seen := make([][]bool, len(graph))
-	for row := range seen {
-		seen[row] = make([]bool, len(graph[0]))
-	}
-
-	// run a DFS in each graph cell
-	for x := 0; x < len(graph); x++ {
-		for y := 0; y < len(graph[0]); y++ {
-			if seen[x][y] {
-				continue
-			}
-			if dfs(graph, seen, x, y, 1) >= 4 {
+	// rows
+	for _, seq := range seqs {
+		for _, rep := range getPossibleRepetitions() {
+			if strings.Contains(seq, rep) {
 				validSeqs++
 			}
 		}
 	}
 
+	// transpose for columns
+	// secondaryDiag for secondary diagonal values
+	// TODO: create principalDiag and add parallelism
+
 	return validSeqs
 }
 
-// TODO: check for direction change. If it changes, return counter and reset last seen
-func dfs(graph [][]string, seen [][]bool, x, y, counter int) int {
-	if seen[x][y] {
-		return counter
+func getPossibleRepetitions() []string {
+	return []string{
+		"BBBB",
+		"UUUU",
+		"DDDD",
+		"HHHH",
 	}
-	seen[x][y] = true
+}
 
-	// traverse in all eight directions (top, bottom, left, right and 4 diagonals)
-	for _, direction := range getDirections() {
-		dx, dy := direction[0], direction[1]
-		if isValidBoundary(x+dx, y+dy, len(graph)) {
-			if graph[x][y] == graph[x+dx][y+dy] {
-				counter++
-				dfs(graph, seen, x+dx, y+dy, counter)
-			}
+func transpose(seqs []string) []string {
+	transposed := []string{}
+	str := ""
+	for i := 0; i < len(seqs); i++ {
+		for _, seq := range seqs {
+			str = str + string(seq[i])
 		}
+		transposed = append(transposed, str)
+		str = ""
 	}
-	return counter
+	return transposed
 }
 
-func getDirections() [][]int {
-	return [][]int{
-		{-1, 0},
-		{1, 0},
-		{0, -1},
-		{0, 1},
-		{-1, -1},
-		{1, 1},
-		{-1, 1},
-		{1, -1},
+func secondaryDiag(seqs []string) []string {
+	secDiag := []string{}
+	arrSize := len(seqs)
+	str := ""
+	for k := arrSize - (arrSize - 4) - 1; k < arrSize-1; k++ {
+		i := k
+		j := 0
+		for {
+			if i < 0 {
+				break
+			}
+			str = str + string(seqs[i][j])
+			i = i - 1
+			j = j + 1
+		}
+		secDiag = append(secDiag, str)
+		str = ""
 	}
-}
 
-func isValidBoundary(x, y, graphLen int) bool {
-	return x < 0 || y < 0 || x >= graphLen || y >= graphLen
+	str = ""
+	for i := 0; i < arrSize; i++ {
+		str = str + (string(seqs[i][i]))
+	}
+	secDiag = append(secDiag, str)
+	str = ""
+
+	for k := 1; k <= arrSize-4; k++ {
+		i := arrSize - 1
+		j := k
+		for {
+			if j > arrSize-1 {
+				break
+			}
+			str = str + (string(seqs[i][j]))
+			i = i - 1
+			j = j + 1
+		}
+		secDiag = append(secDiag, str)
+		str = ""
+	}
+
+	return secDiag
 }
